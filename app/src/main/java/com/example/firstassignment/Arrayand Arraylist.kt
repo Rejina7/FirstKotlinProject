09f2814f8fb61cc43282030f6490ada26444ae7f
regina <reejina06@gmail.com>
2025-11-03 22:41:02 +0545

    Array and Arraylist

---
+++ app/src/main/java/com/example/firstassignment/Arrayand Arraylist.kt
@@ -0,0 +1,11 @@
+package com.example.firstassignment
+
+fun main() {
+    val numbers = arrayOf(1, 2, 3)
+    println(numbers[0])
+
+    val fruits = listOf("Apple", "Banana", "Cherry")
+    for (fruit in fruits) {
+        println(fruit)
+    }
+}
