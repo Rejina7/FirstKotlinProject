3f7d3535a4eaa5705af18e5147993dcd513aec2c
regina <reejina06@gmail.com>
2025-11-03 23:03:50 +0545

    for loop

--- app/src/main/java/com/example/firstassignment/Controlflow.kt
+++ app/src/main/java/com/example/firstassignment/Controlflow.kt
@@ -1,15 +1,9 @@
 package com.example.firstassignment
 
 fun main() {
-    val day = 3
-
-    when(day) {
-        1 -> println("Monday")
-        2 -> println("Tuesday")
-        3 -> println("Wednesday")
-        4,5 -> println("Thursday or Friday")
-        in 6..7 -> println("Weekend")
-        else -> println("Invalid day")
+    for (i in 1..5) { // 1 to 5
+        println(i)
     }
 }
+
 
